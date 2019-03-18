715098051941223abc35ddf4f671e847dee0339d
Newton Galiza <newtonjgaliza@gmail.com>
2019-03-18 15:12:36 -0300

    Create test.py

---
+++ FirstTest/test.py
@@ -0,0 +1,23 @@
+from fractions import Fraction
+import unittest
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(sel
+        data = [1,2,3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+    def test_bad_types(self):
+        data = 'orange'
+        with self.assertRaises(TypeError):
+            result = sum(data)
+
+if __name__ ==  '__main__':
+    unittest;main()
